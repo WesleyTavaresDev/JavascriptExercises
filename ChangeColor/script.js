3e0f16421eac82c9120dda8e1fa164f2cbbca299
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2022-11-10 20:38:16 -0300

    random color

--- ChangeColor/script.js
+++ ChangeColor/script.js
@@ -1,3 +1,8 @@
-function setRandomColor() {
+var randomColor = require('randomcolor');
+
+function setColor() {
+  document.getElementById('div-left').style.color = randomColor();
+  document.getElementById('div-center').style.color = randomColor();
+  document.getElementById('div-right').style.color = randomColor();
   
 }
