86a49f177aea95c47da37935f1217d1b9460fb41
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2022-11-11 17:50:31 -0300

    hexadecimal color string now is put on the block

--- ChangeColor/script.js
+++ ChangeColor/script.js
@@ -1,8 +1,18 @@
 var randomColor = require('randomcolor');
 
+
+
 function setColor() {
-  document.getElementById('div-left').style.backgroundColor = randomColor();
-  document.getElementById('div-center').style.backgroundColor = randomColor();
-  document.getElementById('div-right').style.backgroundColor = randomColor();
-  
+  let colors = randomColor({count: 3})
+  const divLeft = document.getElementById('div-left')
+  divLeft.style.backgroundColor = colors[0];
+  divLeft.innerText = colors[0];
+
+  const divCenter = document.getElementById('div-center');
+  divCenter.style.backgroundColor = colors[1];
+  divCenter.innerText = colors[1]
+
+  const divRight = document.getElementById('div-right');
+  divRight.style.backgroundColor = colors[2];
+  divRight.innerText = colors[2]
 }
