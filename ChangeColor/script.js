70d81d0c4d50cd5a4f6b5e9d6f0e4a23c4155502
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2022-11-11 18:33:27 -0300

    hexadecimal code removed

--- ChangeColor/script.js
+++ ChangeColor/script.js
@@ -6,13 +6,10 @@ function setColor() {
   let colors = randomColor({count: 3})
   const divLeft = document.getElementById('div-left')
   divLeft.style.backgroundColor = colors[0];
-  divLeft.innerText = colors[0];
 
   const divCenter = document.getElementById('div-center');
   divCenter.style.backgroundColor = colors[1];
-  divCenter.innerText = colors[1]
 
   const divRight = document.getElementById('div-right');
   divRight.style.backgroundColor = colors[2];
-  divRight.innerText = colors[2]
 }
