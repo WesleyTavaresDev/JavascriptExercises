47ab79c5b498a0374e454eb284d3ec1a6009e1b0
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2022-11-12 00:28:47 -0300

    color hexadecimal code added (again)

--- ChangeColor/script.js
+++ ChangeColor/script.js
@@ -18,13 +18,16 @@ function setColor() {
   }
 
   let colors = randomColor({hue: hue.value, luminosity: luminosityValue(),count: 3})
-
+  
   const divLeft = document.getElementById('div-left')
   divLeft.style.backgroundColor = colors[0];
-
+  
   const divCenter = document.getElementById('div-center');
   divCenter.style.backgroundColor = colors[1];
 
   const divRight = document.getElementById('div-right');
   divRight.style.backgroundColor = colors[2];
+
+  const finalColors = document.getElementById('finalcolors');
+  finalColors.innerText = (`Colors: [${colors}]`);
 }
