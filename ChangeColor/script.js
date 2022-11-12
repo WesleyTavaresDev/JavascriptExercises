3a1e75c07edd0f61958fa89225eeb162382e95aa
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2022-11-12 15:22:48 -0300

    div constants replace by a for..in loop

--- ChangeColor/script.js
+++ ChangeColor/script.js
@@ -2,26 +2,22 @@ var randomColor = require('randomcolor');
 
   
 function setColor() {
+
+  const boxes = document.getElementsByName('box');
+  
   const hue = document.querySelector(".colors");
+  let colors = randomColor({hue: hue.value, luminosity: luminosityValue() ,count: boxes.length})
 
-  let colors = randomColor({hue: hue.value, luminosity: luminosityValue() ,count: 3})
-  
-  const divLeft = document.getElementById('div-left')
-  divLeft.style.backgroundColor = colors[0];
-  
-  const divCenter = document.getElementById('div-center');
-  divCenter.style.backgroundColor = colors[1];
-
-  const divRight = document.getElementById('div-right');
-  divRight.style.backgroundColor = colors[2];
+  for(let i in boxes) {
+    boxes[i].style.backgroundColor = colors[i];
+  }
 
   const finalColors = document.getElementById('finalcolors');
   finalColors.innerText = (`Colors: [${colors}]`);
 }
 
-
 function luminosityValue() {
   const luminosity = document.getElementById('value').value;
-  return luminosity === "0" ? 'dark' : luminosity === "5" ? 'bright' : 'light';
 
+  return luminosity === "0" ? 'dark' : luminosity === "5" ? 'bright' : 'light';
 }
