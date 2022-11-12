f049ced07dc1536e1a84d84c87cb1394896f6218
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2022-11-12 15:26:47 -0300

    Function rename: luminosityValue -> getLuminosityValue

--- ChangeColor/script.js
+++ ChangeColor/script.js
@@ -6,7 +6,7 @@ function setColor() {
   const boxes = document.getElementsByName('box');
   
   const hue = document.querySelector(".colors");
-  let colors = randomColor({hue: hue.value, luminosity: luminosityValue() ,count: boxes.length})
+  let colors = randomColor({hue: hue.value, luminosity: getLuminosityValue() ,count: boxes.length})
 
   for(let i in boxes) {
     boxes[i].style.backgroundColor = colors[i];
@@ -16,7 +16,7 @@ function setColor() {
   finalColors.innerText = (`Colors: [${colors}]`);
 }
 
-function luminosityValue() {
+function getLuminosityValue() {
   const luminosity = document.getElementById('value').value;
 
   return luminosity === "0" ? 'dark' : luminosity === "5" ? 'bright' : 'light';
